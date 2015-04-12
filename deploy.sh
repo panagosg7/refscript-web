f22c4856aaecc6ebeff5aa40407c6c268fffd9b7
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2015-04-11 18:42:12 -0700

    Edits

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo rsync -avzh --chmod=755 --exclude '.*.swp' ./ /var/www/html/
+sudo rsync -avzh --chmod=755 --exclude '.git' --exclude '.*.swp' ./ /var/www/html/
 
 sudo /etc/init.d/lighttpd force-reload
 
